d4045209f251e2ca8a8a6bcab584fd039748b49d
HenryElvis <elvishenry2402@gmail.com>
2026-06-09 09:43:42 +0200

    fix after each import

--- locator-tools/src/test/java/tools/baseTest.java
+++ locator-tools/src/test/java/tools/baseTest.java
@@ -1,6 +1,7 @@
 package tools;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
